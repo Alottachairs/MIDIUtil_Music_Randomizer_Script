af54e5e06d78f3ff3390bd6ccba027cb560eafc3
Alottachairs <adonfields@gmail.com>
2022-05-10 17:54:19 -0500

    Drums
    Added drum parts,  little wonkey and needs some adjustment but it is working!

--- Main.py
+++ Main.py
@@ -1,26 +1,22 @@
-#Alright, this is the next logical step. we are going to create a script
-# that changes tempo, chords, and time signature, every "measure"
-# let's see what we can do i guess..
-
 
 #imports the needed modules the midifile from midiutil
 from midiutil import MIDIFile
 import random as r
 
 #establishes the varibles and "libraries" of notes and time
-arpArray = ([60,64,67,72],[61,65,68,73],
- [62,66,69,74],[63,67,70,75],
- [64,68,71,76],[65,69,72,77],
- [66,70,73,78],[67,71,74,79],
- [68,72,75,80],[57,61,64,69],
- [58,62,65,70],[59,63,66,71],
+arpArray = ([60,64,67,71],[61,65,68,72], #All 12 Major 7th chords
+ [62,66,69,73],[63,67,70,74],
+ [64,68,71,75],[65,69,72,76],
+ [66,70,73,77],[67,71,74,78],
+ [68,72,75,79],[57,61,64,68],
+ [58,62,65,69],[59,63,66,70],
  
- [60,63,67,72],[61,64,68,73],
- [62,65,69,74],[63,66,70,75],
- [64,67,71,76],[65,68,72,77],
- [66,69,73,78],[67,70,74,79],
- [68,71,75,80],[57,60,64,69],
- [58,61,65,70],[59,62,66,71])
+ [60,63,67,70],[61,64,68,71], #All 12 Minor 7th chords
+ [62,65,69,72],[63,66,70,73],
+ [64,67,71,74],[65,68,72,75],
+ [66,69,73,76],[67,70,74,77],
+ [68,71,75,78],[57,60,64,67],
+ [58,61,65,68],[59,62,66,69])
 
 
 track   = 0
@@ -30,6 +26,9 @@ duration= 1
 bNote   = 0
 bDur    = 0
 bTime   = 0
+dNote   = [48,53,53,53,49,53,53,53,57]
+dCount  = 0
+dTime   = 0
 tempo   = 120
 volume  = 100
 
@@ -42,22 +41,30 @@ MyMidi = MIDIFile(4)
 MyMidi.addTempo(track, time, tempo)
 MyMidi.addTrackName(0, 0, "arpArray")
 MyMidi.addTrackName(1, 0, "Bass")
+MyMidi.addTrackName(2, 0, "808 kit")
 
 #Song loop
-while time < 125:
-    key = r.choice(arpArray)
-    meter = r.choice(timeSigs)
-    bDur = len(meter) * 0.25
+while time < 125: # length of the song Each loop = 1 measure
+    key = r.choice(arpArray) # chooses the key of the measure
+    meter = r.choice(timeSigs) # chooses the meter of the measure
+    bDur = len(meter) * 0.25 
     bNote = key[0] - 12
-    MyMidi.addNote(1,1, bNote, bTime, bDur, volume)
+    MyMidi.addNote(1,1, bNote, bTime, bDur, volume) #writes bass note
     bTime += len(meter) * 0.25
-    for notes in meter:
+    for drums in dNote:
+        MyMidi.addNote(2, 2, drums, dTime, 0.25, volume)
+        dCount += 1
+        dTime += 0.25
+        if dCount > len(meter):
+            break      
+    for notes in meter: # Creates arpeggios in key
         pitch = r.choice(key)
         MyMidi.addNote(0,0,pitch, time, 0.25, volume)
         time += 0.25
-    tempo = r.randrange(35, 200)
+    tempo = r.randrange(35, 200) # choose then write new tempo for next measure
     MyMidi.addTempo(track, time, tempo)
+    dCount = 0
     
-
+# Writes the midi file
 with open("test5.mid", "wb") as output_file:
     MyMidi.writeFile(output_file)
